dfd76a9567aa0c7f3086f050b6e917c0e99cab5a
minikampfzwerg <minikampfzwerg@arcor.de>
2013-02-14 23:56:17 +0100

    corrected: there was the exec of pkgfile (which was there for testing use), now its back to pacman.

--- pacnotify.sh
+++ pacnotify.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-. /etc/pacnotify.conf
+#. /etc/pacnotify.conf
 . /home/$USER/.config/pacnotify
 while [ true ]
  do
-  sleep $updateinterval && /usr/bin/pkgfile gedit > /tmp/qqu && notify-send "Updates" "<b>Anzahl: `grep -c [a-z] /tmp/qqu`</b>\n`cat -b /tmp/qqu` " -t $showinterval
+  sleep $updateinterval && /usr/bin/pacman -Qqu > /tmp/qqu && notify-send "Updates" "<b>Anzahl: `grep -c [a-z] /tmp/qqu`</b>\n`cat -b /tmp/qqu` " -t $showinterval
  done
