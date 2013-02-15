9bfd5718a86f66ced6385ab2c23e5e3af77e08b6
minikampfzwerg <minikampfzwerg@arcor.de>
2013-02-15 20:37:28 +0100

    enhanced readability for notifications

--- pacnotify.sh
+++ pacnotify.sh
@@ -7,16 +7,16 @@ conffile1=/etc/pacnotify.conf
 conffile2=/home/$USER/.config/pacnotify.conf
 #that was all about config
 #lets start
-notify-send Pacnotify "Starting up and setting defaults.\n<b>Sleep: </b>$sleeptime \n<b>Show: </b>$expiretime ms" -t 600000
-if [ -f $conffile1 ];
+notify-send Pacnotify "Starting up …\n\t and setting defaults.\n<b>Sleep: </b>$sleeptime \n<b>Show: </b>$expiretime ms" -t 600000
+if [ -r $conffile1 ];
 	then
 		. $conffile1
 		notify-send Pacnotify "found: global conffile \n<b>Sleep: </b>$sleeptime \n<b>Notify: </b>$expiretime ms" -t 600000
 	else
-		notify-send Pacnotify "global conffile not found. \n<b>Sleep: </b>$sleeptime \n<b>Notify: </b>$expiretime ms" -t 600000
+		notify-send Pacnotify "global conffile <b>not</b> found. \n<b>Sleep: </b>$sleeptime \n<b>Notify: </b>$expiretime ms" -t 600000
 fi
 
-if [ -f $conffile2 ];
+if [ -r $conffile2 ];
 	then
 		. $conffile2
 		notify-send Pacnotify "found: $USER\'s conffile \n<b>Sleep: </b>$sleeptime \n<b>Notify: </b>$expiretime ms \n 	… going background" -t 600000
