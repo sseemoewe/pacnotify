031c223a73cba726c27f75a76a993ba89d02d0eb
minikampfzwerg <minikampfzwerg@arcor.de>
2013-02-15 01:01:30 +0100

    changed: config file is now suffixed .conf

--- pacnotify.sh
+++ pacnotify.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #. /etc/pacnotify.conf
-. /home/$USER/.config/pacnotify
+. /home/$USER/.config/pacnotify.conf
 while [ true ]
  do
   sleep $sleeptime && /usr/bin/pacman -Qqu > /tmp/qqu && notify-send "Updates" "<b>Anzahl: `grep -c [a-z] /tmp/qqu`</b>\n`cat -b /tmp/qqu` " -t $expiretime
