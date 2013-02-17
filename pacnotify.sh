0c701483024481337d48fdd42cb54acd84d94cee
Stephan Seemöwe <sseemoewe@gmail.com>
2013-02-17 14:22:07 +0100

    added bash-case to decide which notify-send to use on start up

--- pacnotify.sh
+++ pacnotify.sh
@@ -3,27 +3,30 @@
 #Setting standards
 sleeptime=1h
 expiretime=600000
+globalconf=/etc/pacnotify.conf
+userconf=/home/$USER/.config/pacnotify.conf_
 icon=$PWD/logo32.png
-conffile1=/etc/pacnotify.conf
-conffile2=/home/$USER/.config/pacnotify.conf
 #that was all about config
 #lets start
-notify-send Pacnotify "Starting up …\n\t and setting defaults.\n<b>Sleep: </b>$sleeptime \n<b>Show: </b>$expiretime ms" -t 600000
-if [ -r $conffile1 ];
+
+if [ -f $userconf ];
 	then
-		. $conffile1
-		notify-send Pacnotify "found: global conffile \n<b>Sleep: </b>$sleeptime \n<b>Notify: </b>$expiretime ms" -t 600000
-	else
-		notify-send Pacnotify "global conffile <b>not</b> found. \n<b>Sleep: </b>$sleeptime \n<b>Notify: </b>$expiretime ms" -t 600000
+		. $userconf
+		initmsg=1
+	elif [ -f $globalconf ];
+		then
+			. $globalconf
+			initmsg=2
+		else
+			initmsg=3
 fi
-if [ -r $conffile2 ];
-	then
-		. $conffile2
-		notify-send Pacnotify "found: $USER\'s conffile \n<b>Sleep: </b>$sleeptime \n<b>Notify: </b>$expiretime ms \n 	… going background" -t 600000
-	else
-		notify-send Pacnotify "$USER\'s confile not found. \n<b>Sleep: </b>$sleeptime \n<b>Notify: </b>$expiretime" -t 600000
-fi
+
+case $initmsg in
+	1) notify-send Pacnotify "Starting up and setting defaults from userconf.\n<b>Sleep: </b>$sleeptime \n<b>Show: </b>$expiretime ms" -t 60000 -i $icon ;;
+	2) notify-send Pacnotify "Starting up and setting defaults from globalconf.\n<b>Sleep: </b>$sleeptime \n<b>Show: </b>$expiretime ms" -t 60000 -i $icon ;;
+	3) notify-send Pacnotify "Starting up and setting defaults.\n<b>Sleep: </b>$sleeptime \n<b>Show: </b>$expiretime ms" -t 60000 -i $icon ;;
+esac
 while [ true ]
  do
-  sleep $sleeptime && /usr/bin/pacman -Qqu > /tmp/qqu && notify-send "Updates" "<b>Anzahl: `grep -c [a-z] /tmp/qqu`</b>\n`cat -b /tmp/qqu` " -t $expiretime -i $icon
+  sleep $sleeptime && /usr/bin/pacman -Qqu > /tmp/qqu && notify-send "Updates" "<b>Anzahl: `grep -c [a-z] /tmp/qqu`</b>\n`cat -b /tmp/qqu` " -t $expiretime
  done
