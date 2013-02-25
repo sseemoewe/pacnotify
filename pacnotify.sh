498eb289eac013ebb3b319a18ca9aa1dd7e03564
Stephan Seemöwe <sseemoewe@gmail.com>
2013-02-25 18:42:53 +0100

    added initial messages. pacnotify now exits if tmpfile exists (well not so nice, because i don't delete it on clean exit)

--- pacnotify.sh
+++ pacnotify.sh
@@ -6,9 +6,10 @@ expiretime=600000
 globalconf=/etc/pacnotify.conf
 userconf=/home/$USER/.config/pacnotify.conf
 icon=$PWD/logo32.png
+tmpfile=/tmp/pacnotify.qqu
 #that was all about config
-#lets start
-
+#lets start by getting the .conf
+#case [ -z $1 ]
 if [ -f $userconf ];
 	then
 		. $userconf
@@ -21,12 +22,19 @@ if [ -f $userconf ];
 			initmsg=3
 fi
 
+# see if its already running
+if [ -f $tmpfile ];
+	then
+		initmsg=4
+fi
+
 case $initmsg in
 	1) notify-send Pacnotify "Starting up and setting defaults from userconf.\n<b>Sleep: </b>$sleeptime \n<b>Show: </b>$expiretime ms" -t 60000 -i $icon ;;
 	2) notify-send Pacnotify "Starting up and setting defaults from globalconf.\n<b>Sleep: </b>$sleeptime \n<b>Show: </b>$expiretime ms" -t 60000 -i $icon ;;
 	3) notify-send Pacnotify "Starting up and setting defaults.\n<b>Sleep: </b>$sleeptime \n<b>Show: </b>$expiretime ms" -t 60000 -i $icon ;;
+	4) notify-send Pacnotify "Seems that pacnotify is already running. If not delete $tmpfile." && exit;;
 esac
 while [ true ]
  do
-  sleep $sleeptime && /usr/bin/pacman -Qqu > /tmp/qqu && notify-send "Updates" "<b>Anzahl: `grep -c [a-z] /tmp/qqu`</b>\n`cat -b /tmp/qqu` " -t $expiretime
+  sleep $sleeptime && /usr/bin/pacman -Qqu > $tmpfile && notify-send "Updates" "<b>Anzahl: `grep -c [a-z] $tmpfile`</b>\n`cat -b $tmpfile` " -t $expiretime
  done
