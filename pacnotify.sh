1f6c6e39b81134a528718f2f4512d2b526e05376
Stephan Seemöwe <sseemoewe@gmail.com>
2013-03-06 19:37:27 +0100

    did some stuff: effective checking for pacman better notifications delay if pacman is running limited output lines (9 lines because it looks best) number of packages not shown in list maybe some more changes i forgot now

--- pacnotify.sh
+++ pacnotify.sh
@@ -6,13 +6,19 @@
 #and fill in the values you want to override. Notice that Users 
 #jconfiguration files always have precedence, then the global one and at 
 #last the values set here.
-sleeptime=1h
-expires=120
+sleeptime=1h	# time to wait between notifications
+expires=120		# seconds to display the notification
+backoff=5		# wait x if pacman is running (in minutes) , start with a small number!
+backoff_limit=30 # limit minutes to wait (per try)
+delay=5			# add x minutes delay to backoff per try
+maxtries=10		# max tries until notification is send anyway, set to 0 to turn waiting off.
+lines=9			# lines of packages to display
+icon=/usr/share/icons/pacnotify32.png # which icon to use
+
+#next few vars should not be touched
 globalconf=/etc/pacnotify.conf
 userconf=/home/$USER/.config/pacnotify.conf
-icon=/usr/share/icons/pacnotify32.png
 tmpfile=/tmp/pacnotify.qqu
-lines=9
 pid=$$
 lockfile=/tmp/pacnotify.lock
 
@@ -29,41 +35,72 @@ if [ -f $userconf ];
 fi
 
 if [ -f $lockfile ];
-				then
-					initmsg=4
-				else
-					echo $pid > $lockfile
+	then
+		initmsg=4
+	else
+		echo $pid > $lockfile
 fi
 
+time=$(($expires * 1000))
 case $initmsg in
-	1) notify-send Pacnotify "Starting up and setting defaults from userconf.\n<b>Sleep: </b>$sleeptime \n<b>Show: </b>$expires s" -t $(($expires * 1000)) -i $icon ;;
-	2) notify-send Pacnotify "Starting up and setting defaults from globalconf.\n<b>Sleep: </b>$sleeptime \n<b>Show: </b>$expires s" -t $(($expires * 1000)) -i $icon ;;
-	3) notify-send Pacnotify "Starting up and setting defaults.\n<b>Sleep: </b>$sleeptime \n<b>Show: </b>$expires s" -t $(($expires * 1000)) -i $icon ;;
-	4) notify-send Pacnotify "Seems that pacnotify is already running. With PID: <b>`cat $lockfile`</b>. If not delete <b>$lockfile</b>.\n" && exit;;
+	1) notify-send Pacnotify "Starting up and setting defaults from userconf.\n<b>Sleep: </b>$sleeptime \n<b>Show: </b>$expires s" -t $time -i $icon ;;
+	2) notify-send Pacnotify "Starting up and setting defaults from globalconf.\n<b>Sleep: </b>$sleeptime \n<b>Show: </b>$expires s" -t $time -i $icon ;;
+	3) notify-send Pacnotify "Starting up and setting defaults.\n<b>Sleep: </b>$sleeptime \n<b>Show: </b>$expires s" -t $time -i $icon ;;
+	4) notify-send Pacnotify "Seems that pacnotify is already running. With PID: <b>`cat $lockfile`</b>. If not delete <b>$lockfile</b>.\n" -t $time -i $icon 
+	exit;;
 esac
 while [ true ]
 	do
-		pacman_pid=`pgrep -f "/usr/bin/pacman -S"`
-		if [ $pacman_pid != 0 ]
-			then
-				echo $pacman_pid
-				pruns='\n<b>pacman is running (PID: '$pacman_pid')</b>\nnumber of updates maybe incorrect.'
-		fi
+		counts=0
+		oldbackoff=$backoff
+		waited=0
+		while [ $counts -lt $maxtries ]
+			do
+			pacman_pid=`pgrep -f "pacman -S"`
+			if [ $pacman_pid != 0 ];
+				then
+					p_runs_wait='<b>pacman is running (PID: '$pacman_pid')</b>\nnumber of updates may be incorrect. Waiting '$backoff'm for pacman to exit.'
+					try=$(($counts + 1))
+					notify-send Pacnotify "$p_runs_wait (Try: $try)" -t $time -i $icon
+					ptrue=0
+				else
+					ptrue=1
+			fi
+			if [ $ptrue -lt 1 ];
+				then
+					sleep $backoff'm'
+					counts=$(( $counts + 1 ))
+					waited=$(( $backoff + $waited))
+					backoff=$(( $backoff + $delay ))
+					if [ $backoff -gt $backoff_limit ];
+						then
+							backoff=$backoff_limit
+					fi
+				else
+					maxtries=-1
+			fi
+			if [ $waited -gt 0 ]
+				then
+					p_runs_skip='<b>pacman was running </b>\nnumber of updates may be incorrect.\nWaited '$waited'm for pacman to exit. Gave up…\n'
+				else
+					p_runs_skip=
+			fi
+		done
 	/usr/bin/pacman -Qqu > $tmpfile
 	x_updates=`grep -c [a-z] $tmpfile`
 	x_diff=$(($x_updates - $lines))
 	if [ $x_diff -lt 0 ];
 		then
 			x_diff=0
-			more=$pruns
 		else
-			more='\n<b>and '$x_diff' more</b>'$pruns
+			more='\n<b>and '$x_diff' more</b>'
 	fi
 	pakete=`head -n $lines $tmpfile|cat -b`
-	notify-send Pacnotify "<b>$x_updates Updates</b>\n$pakete $more" -t $(($expires * 1000)) -i $icon
+	notify-send Pacnotify "$p_runs_skip<b>$x_updates Updates</b>\n$pakete $more" -t $time -i $icon
 	rm $tmpfile
 	pacman_pid=
 	pruns=
 	more=
+	backoff=$oldbackoff
 	sleep $sleeptime
 done
